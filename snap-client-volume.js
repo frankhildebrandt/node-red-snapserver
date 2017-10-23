df5c8fbe22f6e2f081857f47200fc4d7f6799ed1
frank <hildebrandt.io>
2017-10-23 07:20:16 +0200

    added the ability to change the node name

--- snap-client-volume.js
+++ snap-client-volume.js
@@ -7,10 +7,23 @@ module.exports = function (RED) {
         var node = this;
 
         var server = RED.nodes.getNode(config.server);
+        var volume = 100;
+        var muted = false;
 
         node.on('input', function (msg) {
             var client = new net.Socket();
             client.connect(server.port || 1705, server.host, function () {
+
+                if (msg.payload === "MUTE") {
+                    msg.payload = {"muted": true}
+                    muted = true;
+                } else if (msg.payload === "UNMUTE") {
+                    msg.payload = {"muted": false}
+                    muted = false;
+                } else if (!isNaN(msg.payload)) {
+                    volume = parseInt(msg.payload);
+                    msg.payload = {"volume": parseInt(msg.payload)}
+                }
                 var data = {
                     "id": 1, "jsonrpc": "2.0", "method": "Client.SetVolume", "params": {
                         "id": config.id || msg.payload.id,
@@ -20,6 +33,10 @@ module.exports = function (RED) {
                         }
                     }
                 };
+                if (data.params.volume.percent) {
+                    volume = data.params.volume.percent;
+                }
+                node.status({fill: "green", shape: "ring", text: "Muted: " + muted + " Volume: " + volume});
                 client.write('                ' + JSON.stringify(data) + ' \n');
             });
 
